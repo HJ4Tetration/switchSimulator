2e844583661a87646caf81d31b6647702c74bbd0
harojian <harojian@tetrationanalytics.com>
2018-08-24 12:10:19 -0700

    all files added

--- registration/message.go
+++ registration/message.go
@@ -1,1 +1,73 @@
 package registration
+
+type SwitchRegistration struct {
+	Serial string `json:"serial"`
+	Crt    string `json:"crt"`
+}
+
+type SwitchMessage struct {
+	Cmd      string `json:"cmd"`
+	SwitchID string `json:"switchId"`
+}
+
+type ServerMessage struct {
+	ResponseCode int    `json:"responseCode"`
+	Cmd          string `json:"cmd"`
+	// Data: ignore data field, use more specific structs if Data is needed
+}
+
+type SwitchCheckInMessage struct {
+	Cmd      string `json:"cmd"`
+	SwitchID string `json:"switchId"`
+	Data     struct {
+		AgentVersion string `json:"agentVersion"`
+		Capability   string `json:"capability"`
+		GatewayUUID  string `json:"gateway_uuid"`
+		ImageName    string `json:"imageName"`
+		IP           string `json:"ip"`
+		ModTs        string `json:"modTs"`
+		Role         string `json:"role"`
+		State        string `json:"state"`
+		Status       string `json:"status"`
+		SwitchName   string `json:"switch_name"`
+		SystemUpTime string `json:"systemUpTime"`
+	} `json:"data"`
+}
+
+type CollectorBucket struct {
+	Lo        int    `json:"lo"`
+	Hi        int    `json:"hi"`
+	Primary   string `json:"primary"`
+	Secondary string `json:"secondary"`
+}
+
+type CollectorMessage struct {
+	Decommissioned bool   `json:"decommissioned"`
+	IP             string `json:"ip"`
+	Name           string `json:"name"`
+	UpdatedAt      int    `json:"updated_at"`
+	CollectorID    int    `json:"collector_id"`
+	Healthy        bool   `json:"healthy"`
+	SpineUDPPort   int    `json:"spine_udp_port"`
+	UDPPort        int    `json:"udp_port"`
+}
+
+type ServerConfigMessage struct {
+	ResponseCode int    `json:"responseCode"`
+	Cmd          string `json:"cmd"`
+	Data         struct {
+		Buckets         []CollectorBucket  `json:"buckets"`
+		Active          []CollectorMessage `json:"active"`
+		Deactivated     []CollectorMessage `json:"deactivated"`
+		DataPathDisable bool               `json:"dataPathDisable"`
+		CfgOpts         struct {
+			ExportIntervalMs int `json:"exportIntervalMs"`
+		} `json:"cfgOpts"`
+		HwSensors []struct {
+			Dn         string `json:"dn"`
+			ExporterID int    `json:"exporter_id"`
+			SrcPort    int    `json:"src_port"`
+			State      string `json:"state"`
+		} `json:"hwSensors"`
+	} `json:"data"`
+}
--- registration/registration.go
+++ registration/registration.go
@@ -2,14 +2,18 @@ package registration
 
 import(
 	"flag"
-	"sync"
 	"github.com/golang/glog"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"net/url"
+	"crypto/tls"
+	"time"
+	"bytes"
+	"encoding/json"
 )
 
 type switchWebHandler struct{
+	switchName string
 	gatewayRegisterURL url.URL
 	gatewayWssURL url.URL
 	httpClient *http.Client
@@ -28,24 +32,57 @@ func (gateway *gateWay)getGatewayWebsocketIP() string{
 	return gateway.IP
 }
 
-func NewSwitchWebHandler(gateway *gateWay) *switchWebHandler{
+func NewSwitchWebHandler(gateway *gateWay,switchName string) *switchWebHandler{
+	httpClient:=&http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify:true,
+			},
+		},
+	}
+	websocketDialer:=websocket.Dialer{
+		HandshakeTimeout: 60*time.Minute,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify:true,
+		},
+	}
 	return &switchWebHandler{
+		switchName: switchName,
 		gatewayRegisterURL: url.URL{Scheme:"https",Host:gateway.getGatewayRegisterIP(),Path:"/switch_register"},
-		gatewayWssURL: url.URL{Scheme:"wss",Host:gateway.getGatewayWebsocketIP(),Path:"/switch_wss"},	
+		gatewayWssURL: url.URL{Scheme:"wss",Host:gateway.getGatewayWebsocketIP(),Path:"/switch_wss"},
+		httpClient:httpClient,
+		websocketDialer: websocketDialer,
 	}
 }
 
 
 
-
-
-func WebSocketRequest(){
-
+func (s *switchWebHandler)WebSocketRequest(switchName string) bool{//return false if websocket creation fails
+	con,_,err:=s.websocketDialer.Dial(s.gatewayWssURL.String(),nil)
+	if err!=nil{
+		glog.Errorf("Can't make websocket for switch "+s.switchName+": %v\n",err)
+		return false
+	}
+	glog.Infof("Websocket established for switch "+s.switchName+"\n")
+	switchCheckInMessage:=""//getCheckInMessage()
+	switchConfigMessage:=""//getConfigMessage()
+	return true
 }
 
 
-func httpsRequest(){
-
+func (s *switchWebHandler)httpsRequest() bool{//return false if registration fails
+	switchRegistration:=SwitchRegistration{Serial:s.switchName,Crt:""}//Solenoid replaces its cert with switch cert
+	jsonSwitchRegistration,err:=json.Marshal(switchRegistration)
+	if err!=nil{
+		glog.Errorf("can't marshal https registration request for switch "+s.switchName+": %v\n",err)
+		return false
+	}
+	_,err=s.httpClient.Post(s.gatewayRegisterURL.String(),"application/json",bytes.NewReader(jsonSwitchRegistration))
+	if err!=nil{
+		glog.Errorf("error getting https response for switch "+s.switchName+": %v\n",err)
+		return false
+	}
+	return true
 }
 
 
@@ -53,11 +90,19 @@ func main(){
 	flag.Parse()
 	flag.Lookup("logtostderr").Value.Set("true")
 	gateway:=gateWay{"172.21.92.97"}
-	var wg sync.WaitGroup
-	numberOfSwitch := 1
-	//wg.Add(numberOfSwitch)
-	glog.Infof("Start switch registration for %d switches\n", numberOfSwitch)
-	wg.Wait()
+	numberOfSwitches := 1
+	switchTitle:="harojianSwitchSimulator"
+	s:=make([]*switchWebHandler,numberOfSwitches)
+	glog.Infof("Start switch registration for %d switches\n", numberOfSwitches)
+	for i:=0;i<numberOfSwitches;i++{
+		s[i]=NewSwitchWebHandler(&gateway,switchTitle+string(i))
+		glog.Info("Sending https request for switch "+s[i].switchName+"\n")
+		if !s[i].httpsRequest(){
+			glog.Info("https request for switch "+s[i].switchName+" failed\n")
+		}else{
+			glog.Info("https request for switch "+s[i].switchName+" succeeded\n")
+		}
+	}
 	glog.Infof("Registration procedure all done\n")
 	glog.Infof("Start sending UDP packets\n")
 	for {
