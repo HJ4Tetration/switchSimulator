baeb5846b97604039f6e02d6da64a29a25839017
harojian <harojian@tetrationanalytics.com>
2018-08-24 18:13:08 -0700

    file output added

--- registration/registration.go
+++ registration/registration.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"time"
@@ -61,72 +62,109 @@ func NewSwitchWebHandler(gateway *gateWay, switchName string) *switchWebHandler 
 	}
 }
 
-func (s *switchWebHandler) sender(conn *websocket.Conn, toSender chan channelMessage, senderToValidator chan channelMessage, allToMainLoop chan bool) {
+func (s *switchWebHandler) sender(conn *websocket.Conn, toSender chan channelMessage, senderToValidator chan string, allToMainLoop chan string) {
 	for {
 		m := <-toSender
 		jsonMessage, err := json.Marshal(m.message)
 		if err != nil {
 			glog.Errorf(s.switchName+": Can't marshal "+m.cmd+" message: %v\n", err)
 			//todo: send websocket.Close() message to gateway before conn.Close()
-			allToMainLoop <- true
-			break
+			allToMainLoop <- s.switchName
+			return
 		}
 		conn.WriteMessage(websocket.BinaryMessage, jsonMessage)
 		glog.Infof(s.switchName + ": " + m.cmd + " message sent\n")
 		switch m.cmd {
 		case "switch/check_in":
-			senderToValidator <- m
+			senderToValidator <- m.cmd
 		case "switch/config_msg":
-			senderToValidator <- m
+			senderToValidator <- m.cmd
 		case "switch/add_mapping":
-			senderToValidator <- m
+			senderToValidator <- m.cmd
 		default: //todo: add other response to server's command
 		}
 	}
 }
 
-func (s *switchWebHandler) receiver(conn *websocket.Conn, receiverToValidator chan channelMessage, allToMainLoop chan bool) {
+func (s *switchWebHandler) receiver(conn *websocket.Conn, receiverToValidator chan string, allToMainLoop chan string) {
 	for {
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
 			if messageType == websocket.CloseMessage {
 				glog.Infof(s.switchName + ": websocket.Close() message received, close webocket gracefully\n")
-				allToMainLoop <- true
-				break
+				allToMainLoop <- s.switchName
+				return
 			} else {
 				glog.Errorf(s.switchName+": Can't read websocket message: %v\n", err)
 				//todo: send websocket.Close() message to gateway before conn.Close()
-				allToMainLoop <- true
-				break
+				allToMainLoop <- s.switchName
+				return
 			}
 		}
 		var serverMessage ServerMessage //for the cmd value
 		err = json.Unmarshal(message, &serverMessage)
 		if err != nil {
 			glog.Errorf(s.switchName+": Can't unmarshal websocket message: %v\n", err)
-			allToMainLoop <- true //todo: send websocket.Close() message to gateway before conn.CLose()
-			break
+			allToMainLoop <- s.switchName //todo: send websocket.Close() message to gateway before conn.CLose()
+			return
 		}
+		glog.Infof(s.switchName + ": Server's " + serverMessage.Cmd + " message received\n")
 		switch serverMessage.Cmd {
 		case "switch/check_in":
-			channelMessage := channelMessage{serverMessage, serverMessage.Cmd}
-			receiverToValidator <- channelMessage
+			receiverToValidator <- serverMessage.Cmd
 		case "switch/config_msg":
-			channelMessage := channelMessage{serverMessage, serverMessage.Cmd}
-			receiverToValidator <- channelMessage
+			receiverToValidator <- serverMessage.Cmd
+			/*var serverConfigMessage ServerConfigMessage
+			err = json.Unmarshal(message, &serverConfigMessage)
+			if err != nil {
+				glog.Errorf(s.switchName+": Can't unmarshal switch/config_msg message: %v\n", err)
+				allToMainLoop <- s.switchName
+				return
+			}*/
+			err = ioutil.WriteFile("configMessage", message, 0644)
+			if err != nil {
+				glog.Errorf(s.switchName+": Error writing configMessage to file: %v\n", err)
+				allToMainLoop <- s.switchName
+				return
+			}
 		case "switch/add_mapping":
-			channelMessage := channelMessage{serverMessage, serverMessage.Cmd}
-			receiverToValidator <- channelMessage
+			receiverToValidator <- serverMessage.Cmd
 		default: //todo: add other server's command --> generate response -->forward to sender
 		}
-		glog.Infof(s.switchName + ": switchCheckInMessage OK response received\n")
 	}
 }
 
-func (s *switchWebHandler) validator(senderToValidator chan channelMessage, receiverToValidator chan channelMessage, allToMainLoop chan bool) {
+func (s *switchWebHandler) validator(senderToValidator chan string, receiverToValidator chan string, allToMainLoop chan string) {
+	for {
+		ms := <-senderToValidator
+		timeOut := make(chan bool)
+		go func() {
+			time.Sleep(30 * time.Second)
+			timeOut <- true
+		}()
+		for f := true; f == true; {
+			select {
+			case mr := <-receiverToValidator:
+				if mr == ms {
+					glog.Infof(s.switchName + ": request " + ms + " and response " + mr + " matched\n")
+					f = false
+					break
+				} else {
+					glog.Infof(s.switchName + ": Validation error! request " + ms + " and response " + mr + " unmatched\n")
+					allToMainLoop <- s.switchName
+					return
+				}
+			case <-timeOut:
+				glog.Infof(s.switchName + ": Timeout waiting for " + ms + " response\n")
+				allToMainLoop <- s.switchName
+				return
+			default:
+			}
+		}
+	}
 }
 
-func (s *switchWebHandler) WebSocketRequest() bool { //return false if websocket creation fails
+func (s *switchWebHandler) WebSocketRequest(allToMainLoop chan string) bool { //return false if websocket creation fails
 	conn, _, err := s.websocketDialer.Dial(s.gatewayWssURL.String(), nil)
 	if err != nil {
 		glog.Errorf(s.switchName+": Can't make websocket: %v\n", err)
@@ -136,10 +174,8 @@ func (s *switchWebHandler) WebSocketRequest() bool { //return false if websocket
 	//conn.SetReadDeadline(time.Now().Add(time.Minute))
 
 	toSender := make(chan channelMessage, 10)
-	//toReceiver := make(chan channelMessage, 10)
-	senderToValidator := make(chan channelMessage, 10)
-	receiverToValidator := make(chan channelMessage, 10)
-	allToMainLoop := make(chan bool, 10)
+	senderToValidator := make(chan string, 10)
+	receiverToValidator := make(chan string, 10)
 
 	go s.sender(conn, toSender, senderToValidator, allToMainLoop)
 	go s.receiver(conn, receiverToValidator, allToMainLoop)
@@ -152,12 +188,12 @@ func (s *switchWebHandler) httpsRequest() bool { //return false if registration 
 	switchRegistration := SwitchRegistration{Serial: s.switchName, Crt: ""} //Solenoid replaces its cert with switch cert
 	jsonSwitchRegistration, err := json.Marshal(switchRegistration)
 	if err != nil {
-		glog.Errorf("can't marshal https registration request for switch "+s.switchName+": %v\n", err)
+		glog.Errorf(s.switchName+": Can't marshal https registration request: %v\n", err)
 		return false
 	}
 	_, err = s.httpClient.Post(s.gatewayRegisterURL.String(), "application/json", bytes.NewReader(jsonSwitchRegistration))
 	if err != nil {
-		glog.Errorf("error getting https response for switch "+s.switchName+": %v\n", err)
+		glog.Errorf(s.switchName+": Error getting https reponse: %v\n", err)
 		return false
 	}
 	return true
@@ -170,19 +206,27 @@ func main() {
 	numberOfSwitches := 1
 	switchTitle := "harojianSwitchSimulator"
 	s := make([]*switchWebHandler, numberOfSwitches)
+	c := make(chan string, numberOfSwitches)
 	glog.Infof("Start switch registration for %d switches\n", numberOfSwitches)
+
 	for i := 0; i < numberOfSwitches; i++ {
 		s[i] = NewSwitchWebHandler(&gateway, switchTitle+string(i))
-		glog.Info("Sending https request for switch " + s[i].switchName + "\n")
+		glog.Infof(s[i].switchName + ": Sending https request\n")
 		if !s[i].httpsRequest() {
-			glog.Info("https request for switch " + s[i].switchName + " failed\n")
+			glog.Infof(s[i].switchName + ": https request failed\n")
 		} else {
-			glog.Info("https request for switch " + s[i].switchName + " succeeded\n")
+			glog.Infof(s[i].switchName + ": https request succeeded\n")
+			glog.Infof(s[i].switchName + ": Sending websocket request\n")
+			if !s[i].WebSocketRequest(c) {
+				glog.Infof(s[i].switchName + ": Websocket request failed\n")
+			}
 		}
 	}
 	glog.Infof("Registration procedure all done\n")
-	glog.Infof("Start sending UDP packets\n")
-	for {
-
+	for n := numberOfSwitches; n > 0; {
+		switchName := <-c
+		n--
+		glog.Infof(switchName + ": websocket closed\n")
 	}
+	glog.Infof("All websockets closed, quit main loop\n")
 }
